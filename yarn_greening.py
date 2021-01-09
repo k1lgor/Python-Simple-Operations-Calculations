39e95e2e28c156a7f8ea7c665bb4391dc9f0cdac
k1lgor <65338982+k1lgor@users.noreply.github.com>
2021-01-09 23:48:01 +0200

    Delete yarn_greening.py

--- yarn_greening.py
+++
@@ -1,7 +0,0 @@
-number = float(input())
-cubmeter = 7.61
-discount = 0.18
-sum1 = number * cubmeter * discount
-sum2 = number * cubmeter - sum1
-print(f'The final price is: {sum2:.2f} lv.')
-print(f'The discount is: {sum1:.2f} lv.')
